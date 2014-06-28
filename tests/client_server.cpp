6ed283aa3ae69d2b473a381487c5120fe1aa2ce6
Aaron Hebert <aaron.hebert@gmail.com>
2014-06-28 12:49:20 -0700

    socket lib updates

--- tests/client_server.cpp
+++ tests/client_server.cpp
@@ -9,42 +9,42 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
-#include "tcp_sockets.h"
+#include "tcp.h"
 
 /*
  * Simple C++ Test Suite
  */
 
 std::string test2(std::string text) {
-    std::cout << "client test 2" << std::endl;
+    std::cout << "test2: server read" << std::endl;
 
-    std::cout << "test2 read " << text << std::endl;
+    std::cout << "test2: read " << text << std::endl;
 
-    std::cout << "returning string";
+    std::cout << "test2: returning string\n";
 
-    return "this is a test return";
+    return "this is a test2 server return\t";
 }
 
 void test1() {
     std::cout << "client test 1" << std::endl;
-    TCPSocket tcpServer;
-    TCPSocket tcpClient;
+    log2::tcp::socket tcpServer;
+    log2::tcp::socket tcpClient;
 
     tcpServer.set_read_callback(test2);
-    tcpServer.listen_for_connections("127.0.0.1", "666");
-    tcpServer.set_EOL_char('\n');
+    tcpServer.listen("127.0.0.1", "666");
+    log2::tcp::EOL = '\t';
 
     sleep(5);
 
     tcpClient.connect("127.0.0.1", "666");
     if (tcpClient.connected()) {
-        tcpClient.write("*************write from client test*************\n");
+        tcpClient.write(": from test1: *************write from client test*************\t");
         tcpClient.send();
+        std::cout << "test1: " << tcpClient.readline();
         tcpClient.disconnect();
     }
 
-    tcpServer.kill_service();
-
+    tcpServer.kill();
 }
 
 int main(int argc, char** argv) {
